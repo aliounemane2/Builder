e7b16f999158266b202073493038babc90dce2c6
Alioune MANE <aliounemane@finetechgroupe.com>
2021-12-26 18:09:42 +0000

    Builder

---
+++ src/com/sn/mane/ConcreteBuilder.java
@@ -0,0 +1,32 @@
+package com.sn.mane;
+
+/**
+ * ConcreteBuilder class, constructs and assembles parts of the Product by
+ * implementing the Builder interface
+ */
+public class ConcreteBuilder extends Builder {
+
+    private Product product;
+
+    @Override
+    public Builder createProduct() {
+        this.product = new Product();
+        return this;
+    }
+
+    @Override
+    public Builder buildPart1(String part1) {
+        product.setPart1(part1);
+        return this;
+    }
+
+    @Override
+    public Builder buildPart2(String part2) {
+        product.setPart2(part2);
+        return this;
+    }
+
+    public Product getResult() {
+        return product;
+    }
+}
